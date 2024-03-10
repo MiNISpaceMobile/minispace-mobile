4e921ce0525f28a743a090c978c9aaa27524da23
zeraye <zeraye@protonmail.com>
2024-03-10 17:51:11 +0100

    chore: add `react-native-paper`

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { PaperProvider, Text } from "react-native-paper";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Text>Hello, World!</Text>
+      </View>
+    </PaperProvider>
   );
 }
 
