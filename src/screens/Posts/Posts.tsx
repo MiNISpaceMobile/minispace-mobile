a5d9b562db5ad7d3898e79b5582e2fcbd2ed0c95
zeraye <zeraye@protonmail.com>
2024-04-03 17:21:33 +0200

    feat: add `PostListStack`, `PostDetailsStack` and `FilterStack` to `Posts` screen

--- src/screens/Posts/Posts.tsx
+++ src/screens/Posts/Posts.tsx
@@ -1,11 +1,46 @@
-import { Text } from "react-native-paper";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+import FilterStack from "./FilterStack/FilterStack";
+import PostDetailsStack from "./PostDetailsStack/PostDetailsStack";
+import PostListStack from "./PostListStack/PostListStack";
+
+const Stack = createStackNavigator();
 
 const Posts = () => {
   return (
-    <SafeAreaView>
-      <Text>Posts</Text>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="PostList"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen
+          name="PostList"
+          children={(props) => (
+            <SafeAreaView>
+              <PostListStack {...props} />
+            </SafeAreaView>
+          )}
+        />
+        <Stack.Screen
+          name="PostDetails"
+          children={(props) => (
+            <SafeAreaView>
+              <PostDetailsStack {...props} />
+            </SafeAreaView>
+          )}
+        />
+        <Stack.Screen
+          name="Filter"
+          children={(props) => (
+            <SafeAreaView>
+              <FilterStack {...props} />
+            </SafeAreaView>
+          )}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
