0f5f3e3fce47d5810146f33d698660c3764d49c5
zeraye <zeraye@protonmail.com>
2024-04-13 17:15:40 +0200

    feat: use zustand store in `PostDetailsStack`

--- src/screens/Posts/PostDetailsStack/PostDetailsStack.tsx
+++ src/screens/Posts/PostDetailsStack/PostDetailsStack.tsx
@@ -1,7 +1,9 @@
+import { useEffect } from "react";
 import { View } from "react-native";
 import { Text } from "react-native-paper";
 
 import Post from "../../../interfaces/Post";
+import { usePostDetailsStore } from "../../../zustand/post-details";
 
 interface PostDetailsStackProps {
   route: any;
@@ -11,10 +13,29 @@ interface PostDetailsStackProps {
 const PostDetailsStack = ({ route, navigation }: PostDetailsStackProps) => {
   const { post } = route.params as { post: Post };
 
-  return (
+  const postDetails = usePostDetailsStore((state) => state.postDetails);
+  const error = usePostDetailsStore((state) => state.error);
+  const loading = usePostDetailsStore((state) => state.loading);
+  const fetchPostDetails = usePostDetailsStore(
+    (state) => state.fetchPostDetails,
+  );
+
+  useEffect(() => {
+    fetchPostDetails(post.id);
+  }, []);
+
+  return loading ? (
     <View>
-      <Text>{post.title}</Text>
-      <Text>{post.content}</Text>
+      <Text>Loading...</Text>
+    </View>
+  ) : error || postDetails === null ? (
+    <View>
+      <Text>Error</Text>
+    </View>
+  ) : (
+    <View>
+      <Text>{postDetails.title}</Text>
+      <Text>{postDetails.content}</Text>
     </View>
   );
 };
