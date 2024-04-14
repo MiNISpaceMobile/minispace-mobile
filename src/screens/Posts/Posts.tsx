16172e16b3b5e615f5297c5e6b1ec1c515ba3820
zeraye <zeraye@protonmail.com>
2024-04-14 13:17:23 +0200

    fix: remove `NavigationContainer` from `Posts`
    Currently `NavigationContainer` is located in `MainNavigation` and only one `NavigationContainer` can exists.

--- src/screens/Posts/Posts.tsx
+++ src/screens/Posts/Posts.tsx
@@ -1,4 +1,3 @@
-import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -10,37 +9,35 @@ const Stack = createStackNavigator();
 
 const Posts = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="PostList"
-        screenOptions={{ headerShown: false }}
-      >
-        <Stack.Screen
-          name="PostList"
-          children={(props) => (
-            <SafeAreaView testID="post-list">
-              <PostListStack {...props} />
-            </SafeAreaView>
-          )}
-        />
-        <Stack.Screen
-          name="PostDetails"
-          children={(props) => (
-            <SafeAreaView>
-              <PostDetailsStack {...props} />
-            </SafeAreaView>
-          )}
-        />
-        <Stack.Screen
-          name="Filter"
-          children={(props) => (
-            <SafeAreaView>
-              <FilterStack {...props} />
-            </SafeAreaView>
-          )}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator
+      initialRouteName="PostList"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen
+        name="PostList"
+        children={(props) => (
+          <SafeAreaView testID="post-list">
+            <PostListStack {...props} />
+          </SafeAreaView>
+        )}
+      />
+      <Stack.Screen
+        name="PostDetails"
+        children={(props) => (
+          <SafeAreaView>
+            <PostDetailsStack {...props} />
+          </SafeAreaView>
+        )}
+      />
+      <Stack.Screen
+        name="Filter"
+        children={(props) => (
+          <SafeAreaView>
+            <FilterStack {...props} />
+          </SafeAreaView>
+        )}
+      />
+    </Stack.Navigator>
   );
 };
 
