4d1561a10b22b7d98ae584a9bede49bcf259d7cb
zeraye <zeraye@protonmail.com>
2024-04-14 13:18:53 +0200

    feat: add `PostDetailsReactions`

---
+++ src/screens/Posts/PostDetailsStack/PostDetails/PostDetailsReactions/PostDetailsReactions.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+import PostDetailsReactionsButton from "./PostDetailsReactionsButton/PostDetailsReactionsButton";
+
+export type ReactionType =
+  | null
+  | "thumb-up"
+  | "cards-heart"
+  | "emoticon-lol"
+  | "emoticon-cry";
+
+const PostDetailsReactions = () => {
+  const [selectedReaction, setSelectedReaction] = useState<ReactionType>(null);
+
+  const setSelectedReactionHandler = (newSelectedReaction: ReactionType) => {
+    setSelectedReaction(newSelectedReaction);
+  };
+
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "flex-end",
+        alignItems: "center",
+      }}
+    >
+      <PostDetailsReactionsButton
+        icon="thumb-up"
+        selectedReaction={selectedReaction}
+        setSelectedReactionHandler={setSelectedReactionHandler}
+      />
+      <PostDetailsReactionsButton
+        icon="cards-heart"
+        selectedReaction={selectedReaction}
+        setSelectedReactionHandler={setSelectedReactionHandler}
+      />
+      <PostDetailsReactionsButton
+        icon="emoticon-lol"
+        selectedReaction={selectedReaction}
+        setSelectedReactionHandler={setSelectedReactionHandler}
+      />
+      <PostDetailsReactionsButton
+        icon="emoticon-cry"
+        selectedReaction={selectedReaction}
+        setSelectedReactionHandler={setSelectedReactionHandler}
+      />
+      <Text
+        variant="titleMedium"
+        style={{
+          padding: 10,
+        }}
+      >
+        45
+      </Text>
+    </View>
+  );
+};
+
+export default PostDetailsReactions;
---
+++ src/screens/Posts/PostDetailsStack/PostDetails/PostDetailsReactions/PostDetailsReactionsButton/PostDetailsReactionsButton.tsx
@@ -0,0 +1,33 @@
+import { IconButton } from "react-native-paper";
+import { IconSource } from "react-native-paper/lib/typescript/components/Icon";
+
+import { ReactionType } from "../PostDetailsReactions";
+
+interface PostDetailsReactionsButtonProps {
+  icon: ReactionType;
+  selectedReaction: ReactionType;
+  setSelectedReactionHandler: (newSelectedReaction: ReactionType) => void;
+}
+
+const PostDetailsReactionsButton = ({
+  icon,
+  selectedReaction,
+  setSelectedReactionHandler,
+}: PostDetailsReactionsButtonProps) => {
+  return (
+    <IconButton
+      icon={icon + (icon === selectedReaction ? "" : "-outline")}
+      mode={icon === selectedReaction ? "contained-tonal" : undefined}
+      size={24}
+      onPress={() => {
+        if (icon === selectedReaction) {
+          setSelectedReactionHandler(null);
+        } else {
+          setSelectedReactionHandler(icon);
+        }
+      }}
+    />
+  );
+};
+
+export default PostDetailsReactionsButton;
