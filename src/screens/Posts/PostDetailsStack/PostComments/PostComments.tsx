42e0c7354774aefbf93f5b4c6068b4cd6c615087
zeraye <zeraye@protonmail.com>
2024-04-14 13:18:39 +0200

    feat: add dummy `PostComment`

---
+++ src/screens/Posts/PostDetailsStack/PostComments/PostComments.tsx
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+const PostComments = () => {
+  return (
+    <View>
+      <Text>Post comments</Text>
+    </View>
+  );
+};
+
+export default PostComments;
