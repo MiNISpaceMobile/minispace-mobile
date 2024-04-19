f2c81ddb8b0433ba8df560d3a5eb84d5de255662
zeraye <zeraye@protonmail.com>
2024-04-19 20:04:36 +0200

    feat: add `EventAttributesCheckbox` to `FilerList`

---
+++ src/screens/Posts/FilterStack/FilterList/EventAttributesCheckbox.tsx/EventAttributesCheckbox.tsx
@@ -0,0 +1,47 @@
+import { Checkbox } from "react-native-paper";
+
+import { usePostFiltersStore } from "../../../../../zustand/post-filters";
+import FilterListItem from "../FilterListItem/FilterListItem";
+
+const EventAttributesCheckbox = () => {
+  const filters = usePostFiltersStore((state) => state.filters);
+  const setFilters = usePostFiltersStore((state) => state.setFilters);
+
+  return (
+    <FilterListItem label="Tylko wydarzenia">
+      <Checkbox.Item
+        label="w których biorę udział"
+        status={filters.hasToBeRegisteredForEvent ? "checked" : "unchecked"}
+        onPress={() => {
+          setFilters({
+            ...filters,
+            hasToBeRegisteredForEvent: !filters.hasToBeRegisteredForEvent,
+          });
+        }}
+      />
+      <Checkbox.Item
+        label="w których biorą udział moi znajomi"
+        status={filters.hasToBeRegisteredForEvent ? "checked" : "unchecked"}
+        onPress={() => {
+          setFilters({
+            ...filters,
+            hasToBeRegisteredForEvent: !filters.hasToBeRegisteredForEvent,
+          });
+        }}
+      />
+      {/* TODO: only display if user is organizer */}
+      {/* <Checkbox.Item
+        label="które organizuję"
+        status={filters.hasToBeRegisteredForEvent ? "checked" : "unchecked"}
+        onPress={() => {
+          setFilters({
+            ...filters,
+            hasToBeRegisteredForEvent: !filters.hasToBeRegisteredForEvent,
+          });
+        }}
+      /> */}
+    </FilterListItem>
+  );
+};
+
+export default EventAttributesCheckbox;
