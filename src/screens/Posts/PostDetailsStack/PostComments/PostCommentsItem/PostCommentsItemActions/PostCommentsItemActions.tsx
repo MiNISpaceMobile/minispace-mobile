a12084b2d4f278d62584f732038b5315b291612f
zeraye <zeraye@protonmail.com>
2024-05-08 18:58:48 +0200

    feat: implement report action for comments

--- src/screens/Posts/PostDetailsStack/PostComments/PostCommentsItem/PostCommentsItemActions/PostCommentsItemActions.tsx
+++ src/screens/Posts/PostDetailsStack/PostComments/PostCommentsItem/PostCommentsItemActions/PostCommentsItemActions.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { View } from "react-native";
 import { IconButton, Text } from "react-native-paper";
 
+import ReportDialog from "../../../../../../components/ReportDialog/ReportDialog";
 import PostComment from "../../../../../../interfaces/PostComment";
 
 interface PostCommentsItemActionsProps {
@@ -8,6 +10,8 @@ interface PostCommentsItemActionsProps {
 }
 
 const PostCommentsItemActions = ({ comment }: PostCommentsItemActionsProps) => {
+  const [reportDialogVisible, setReportDialogVisible] = useState(false);
+
   return (
     <View
       style={{
@@ -16,10 +20,21 @@ const PostCommentsItemActions = ({ comment }: PostCommentsItemActionsProps) => {
         alignItems: "center",
       }}
     >
-      <IconButton icon="flag" size={24} onPress={() => {}} />
+      <IconButton
+        icon="flag"
+        size={24}
+        onPress={() => setReportDialogVisible(true)}
+      />
       <IconButton icon="reply" size={24} onPress={() => {}} />
       <IconButton icon="thumb-up-outline" size={24} onPress={() => {}} />
       <Text variant="titleMedium">{comment.likes}</Text>
+      <ReportDialog
+        dialogVisible={reportDialogVisible}
+        hideDialog={() => setReportDialogVisible(false)}
+        label="Napisz w jaki sposób komentarz, który zgłaszasz sprzeczny jest z regulaminem aplikacji:"
+        // TODO: send report request
+        postReport={() => {}}
+      />
     </View>
   );
 };
