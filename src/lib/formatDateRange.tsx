0bf51de95c3b749be9dd4fc4870e8f8869ce3a84
zeraye <zeraye@protonmail.com>
2024-05-17 16:01:35 +0200

    feat: add lib for `formatDateRange`

---
+++ src/lib/formatDateRange.tsx
@@ -0,0 +1,15 @@
+import moment from "moment";
+
+const formatDateRange = (startDate: Date, endDate: Date) => {
+  const start = moment(startDate);
+  const end = moment(endDate);
+
+  if (start.month() === end.month() && start.year() === end.year()) {
+    return `${start.format("D")}-${end.format("D MMMM YYYY")}`;
+  } else if (start.year() === end.year()) {
+    return `${start.format("D MMMM")}-${end.format("D MMMM YYYY")}`;
+  }
+  return `${start.format("D MMMM YYYY")}-${end.format("D MMMM YYYY")}`;
+};
+
+export default formatDateRange;
