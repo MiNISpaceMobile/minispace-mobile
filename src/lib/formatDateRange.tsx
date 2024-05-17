f0834c6ec6b07da6f56e0bd459642ec5ce7723ad
zeraye <zeraye@protonmail.com>
2024-05-17 16:21:35 +0200

    fix: spacing in `formatDateRange` text

--- src/lib/formatDateRange.tsx
+++ src/lib/formatDateRange.tsx
@@ -7,9 +7,9 @@ const formatDateRange = (startDate: Date, endDate: Date) => {
   if (start.month() === end.month() && start.year() === end.year()) {
     return `${start.format("D")}-${end.format("D MMMM YYYY")}`;
   } else if (start.year() === end.year()) {
-    return `${start.format("D MMMM")}-${end.format("D MMMM YYYY")}`;
+    return `${start.format("D MMMM")} - ${end.format("D MMMM YYYY")}`;
   }
-  return `${start.format("D MMMM YYYY")}-${end.format("D MMMM YYYY")}`;
+  return `${start.format("D MMMM YYYY")} - ${end.format("D MMMM YYYY")}`;
 };
 
 export default formatDateRange;
