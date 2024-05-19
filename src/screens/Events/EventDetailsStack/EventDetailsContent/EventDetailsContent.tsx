334690a09caf75aba33ddc57317435d6a6221e3e
zeraye <zeraye@protonmail.com>
2024-05-19 14:49:04 +0200

    fix: disallow tab change while loading

--- src/screens/Events/EventDetailsStack/EventDetailsContent/EventDetailsContent.tsx
+++ src/screens/Events/EventDetailsStack/EventDetailsContent/EventDetailsContent.tsx
@@ -1,37 +1,45 @@
 import { useState } from "react";
 import { View } from "react-native";
-import { Text } from "react-native-paper";
 
 import EventDetailsContentDetails from "./EventDetailsContentDetails/EventDetailsContentDetails";
 import EventDetailsContentTabLabel from "./EventDetailsContentTabLabel/EventDetailsContentTabLabel";
+import { useEventDetailsStore } from "../../../../zustand/event-details";
 
 type TabLabel = "details" | "comments" | "posts";
 
 const EventDetailsContent = () => {
   const [tabLabel, setTabLabel] = useState<TabLabel>("details");
 
+  const loading = useEventDetailsStore((state) => state.loading);
+
   return (
     <View>
-      <View style={{ flexDirection: "row", marginTop: 10, marginBottom: 20 }}>
+      <View style={{ flexDirection: "row", marginTop: 5, marginBottom: 15 }}>
         <EventDetailsContentTabLabel
           label="Szczegóły"
           selected={tabLabel === "details"}
           onPress={() => {
-            setTabLabel("details");
+            if (!loading) {
+              setTabLabel("details");
+            }
           }}
         />
         <EventDetailsContentTabLabel
           label="Komentarze"
           selected={tabLabel === "comments"}
           onPress={() => {
-            setTabLabel("comments");
+            if (!loading) {
+              setTabLabel("comments");
+            }
           }}
         />
         <EventDetailsContentTabLabel
           label="Posty"
           selected={tabLabel === "posts"}
           onPress={() => {
-            setTabLabel("posts");
+            if (!loading) {
+              setTabLabel("posts");
+            }
           }}
         />
       </View>
