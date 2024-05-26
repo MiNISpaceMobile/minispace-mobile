6d2a34184a69aea156bc50473b396ae7ce8de3f7
zeraye <zeraye@protonmail.com>
2024-05-26 15:13:46 +0200

    feat: add list for friend requests received

--- src/screens/Account/FriendsStack/FriendRequestsReceived/FriendRequestsReceived.tsx
+++ src/screens/Account/FriendsStack/FriendRequestsReceived/FriendRequestsReceived.tsx
@@ -1,11 +1,45 @@
-import { View } from "react-native";
-import { Text } from "react-native-paper";
+import RenderItem from "./RenderItem/RenderItem";
+import StoreFlatList from "../../../../components/StoreFlatList/StoreFlatList";
+import { useFriendRequestsReceivedStore } from "../../../../zustand/friend-requests-received";
 
-const FriendRequestsReceived = () => {
+interface FriendRequestsReceivedProps {
+  route: any;
+  navigation: any;
+}
+
+const FriendRequestsReceived = ({
+  route,
+  navigation,
+}: FriendRequestsReceivedProps) => {
+  const friendRequestsReceived = useFriendRequestsReceivedStore(
+    (state) => state.friendRequestsReceived,
+  );
+  const refresh = useFriendRequestsReceivedStore((state) => state.refresh);
+  const fetchNextPage = useFriendRequestsReceivedStore(
+    (state) => state.fetchFriendRequestsReceived,
+  );
+  const loading = useFriendRequestsReceivedStore((state) => state.loading);
+  const error = useFriendRequestsReceivedStore((state) => state.error);
+  const isLastPage = useFriendRequestsReceivedStore(
+    (state) => state.isLastPage,
+  );
+  const page = useFriendRequestsReceivedStore((state) => state.page);
+
   return (
-    <View>
-      <Text>FriendRequestsSent</Text>
-    </View>
+    <StoreFlatList
+      route={route}
+      navigation={navigation}
+      refresh={refresh}
+      fetchNextPage={fetchNextPage}
+      loading={loading}
+      error={error}
+      list={friendRequestsReceived}
+      isLastPage={isLastPage}
+      page={page}
+      errorLabel="Problem z wczytaniem danych."
+      emptyListLabel="Nie znaleziono żadnego zaproszenia."
+      RenderItem={RenderItem}
+    />
   );
 };
 
