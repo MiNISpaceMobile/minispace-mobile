eae6791f10cd49ecae9e4b4c7ea7c520a09df7b0
zeraye <zeraye@protonmail.com>
2024-05-28 21:22:38 +0200

    feat: add create event zustand store

--- src/zustand/create-event.ts
+++ src/zustand/create-event.ts
@@ -1,19 +1,40 @@
 import axios, { AxiosError } from "axios";
 import * as SecureStore from "expo-secure-store";
+import moment from "moment";
 import { create } from "zustand";
 
 import IEventCreator from "../interfaces/EventCreator";
 
 interface CreateEventState {
+  eventCreator: IEventCreator;
   loading: boolean;
   error: AxiosError | null;
-  createEvent: (eventCreator: IEventCreator) => Promise<void>;
+  updateEventCreator: (eventCreator: IEventCreator) => void;
+  createEvent: () => Promise<void>;
+  hasErrors: () => boolean;
+  clear: () => void;
 }
 
+const defaultEventCreator: IEventCreator = {
+  title: "",
+  description: "",
+  publicationDate: moment().toDate(),
+  startDate: moment().toDate(),
+  endDate: moment().add(1, "days").toDate(),
+  location: "",
+  capacity: 0,
+  fee: 0,
+  picture: null,
+};
+
 export const useCreateEventStore = create<CreateEventState>((set, get) => ({
+  eventCreator: defaultEventCreator,
   loading: false,
   error: null,
-  createEvent: async (eventCreator: IEventCreator) => {
+  updateEventCreator: (eventCreator: IEventCreator) => {
+    set({ eventCreator });
+  },
+  createEvent: async () => {
     set({ loading: true });
 
     const jwt = await SecureStore.getItemAsync("jwt");
@@ -27,16 +48,17 @@ export const useCreateEventStore = create<CreateEventState>((set, get) => ({
 
     await axios({
       url: `/events/create`,
-      method: "get",
+      method: "post",
       baseURL: process.env.EXPO_PUBLIC_API_URL,
       headers: { Authorization: "Bearer " + jwt },
       data: {
-        ...eventCreator,
+        ...get().eventCreator,
         eventCategory: 0, // event category is not used, but need to be passed (will be removed in the future)
+        publicationDate: get().eventCreator.startDate, // currenlty publicationDate is always equal startDate
       },
     })
       .then((response) => {
-        set({ error: null });
+        set({ error: null, eventCreator: defaultEventCreator });
       })
       .catch((error: AxiosError) => {
         set({ error });
@@ -44,5 +66,16 @@ export const useCreateEventStore = create<CreateEventState>((set, get) => ({
       .finally(() => {
         set({ loading: false });
       });
+  },
+  hasErrors: () => {
+    const eventCreator = get().eventCreator;
+    return (
+      eventCreator.title.length === 0 ||
+      eventCreator.location.length === 0 ||
+      eventCreator.capacity === 0
+    );
+  },
+  clear: () => {
+    set({ eventCreator: defaultEventCreator });
   },
 }));
